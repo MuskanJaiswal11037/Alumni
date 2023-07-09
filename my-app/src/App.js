99081a164221de64280d8cfd2c14a2231a285af9
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-07-09 15:49:21 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,10 @@ import {HashRouter,Routes,Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <HashRouter basename='/'>
+    <HashRouter basename='/Alumni'>
       <Routes>
-       <Route path="/Alumni"  exact element ={<Home/>}></Route>
-       <Route path = "/Alumni/About"  element ={<About/>}></Route>
+       <Route path="/"  exact element ={<Home/>}></Route>
+       <Route path = "/About"  element ={<About/>}></Route>
       </Routes>
     </HashRouter>
     </div>
