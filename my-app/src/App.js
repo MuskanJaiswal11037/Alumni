af94e44c3bbe55a0c0139557cb3409e5b96082fc
MuskanJaiswal11037 <102662949+MuskanJaiswal11037@users.noreply.github.com>
2023-07-09 15:47:35 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
 import Home from "./components/Home";
 import About from "./components/About.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {HashRouter,Routes,Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <Router>
+    <HashRouter basename='/'>
       <Routes>
        <Route path="/Alumni"  exact element ={<Home/>}></Route>
        <Route path = "/Alumni/About"  element ={<About/>}></Route>
       </Routes>
-    </Router>
+    </HashRouter>
     </div>
   );
 }
